fe77b5a4f20dbed446a1b50dcac4e9d5a1554d77
João Guilherme <jaaumgui@gmail.com>
2023-04-05 14:31:41 -0300

    Corrigido bug

--- main.c
+++ main.c
@@ -2,52 +2,44 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-
-typedef struct no{
+typedef struct no {
     int dado;
-    struct no * prox;
-}tno;
+    struct no *prox;
+} tno;
 
-typedef struct fila{
-    tno * comeco;
-    tno * fim;
-}tfila;
+typedef struct fila {
+    tno *comeco;
+    tno *fim;
+} tfila;
 
-void criar (tfila * ppf){
+void criar(tfila *ppf) {
     ppf->comeco = NULL;
     ppf->fim = NULL;
 }
 
-bool enfilerar(tfila * ppf, int valor){
-    tno * novo = (tno *) malloc(sizeof (tno));
-    if (novo == NULL){
-    	printf("ERRO");
+bool enfilerar(tfila *ppf, int valor) {
+    tno *novo = (tno *)malloc(sizeof(tno));
+    if (novo == NULL) {
+        printf("ERRO");
         return false;
-    }else {
+    } else {
+        novo->dado = valor;
+        novo->prox = NULL;
         if (ppf->comeco == NULL) {
-            novo->dado = valor;
-            novo->prox = NULL;
             ppf->comeco = novo;
-            ppf->fim = novo;
-            printf("FilaPF[%d]", novo->dado);
-            return true;
-        }else{
-            novo->dado = valor;
-            novo-> prox = NULL;
-            (ppf->fim)->prox = novo;
-            ppf->fim = novo;
-            printf("FilaPF[%d]", novo->dado);
-            return true;
+        } else {
+            ppf->fim->prox = novo;
         }
+        ppf->fim = novo;
+        return true;
     }
 }
 
-bool desinfileirar(tfila * ppf, int * valor){
-    tno * aux;
-    if(ppf->comeco==NULL){
+bool desinfileirar(tfila *ppf, int *valor) {
+    if (ppf->comeco == NULL) {
         return false;
-    }else{
-        aux = ppf->comeco;
+    } else {
+        tno *aux = ppf->comeco;
         *valor = aux->dado;
         ppf->comeco = aux->prox;
         free(aux);
@@ -55,60 +47,54 @@ bool desinfileirar(tfila * ppf, int * valor){
     }
 }
 
-bool separar_impar_par(tfila * fila_inicial, tfila * fila_impar, tfila * fila_par){
-	int valor;
-	while(desinfileirar(fila_inicial, &valor)){
-		if(valor%2==0){
-			if(!enfilerar(fila_par, valor)){
-				return false;
-			}
-			printf("Salvo par %d\n", valor);
-		}else if(valor%2!=0){
-			if(!enfilerar(fila_impar, valor)){	
-				return false;
-			}
-			printf("Salvo impar %d\n", valor);
-		}
-	}
-	return true;	
+bool separar_impar_par(tfila *fila_inicial, tfila *fila_impar, tfila *fila_par) {
+    int valor;
+    while (desinfileirar(fila_inicial, &valor)) {
+        if (valor % 2 == 0) {
+            if (!enfilerar(fila_par, valor)) {
+                return false;
+            }
+        } else {
+            if (!enfilerar(fila_impar, valor)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-
-
-bool imprimir_fila(tfila * fila){
-	int valor;
-	while(desinfileirar(fila, &valor)){
-		printf("%d-", valor);
-	}
-	return true;
+bool imprimir_fila(tfila *fila) {
+    int valor;
+    while (desinfileirar(fila, &valor)) {
+        printf("%d-", valor);
+    }
+    return true;
 }
-
 
 int main() {
-    int pegaValor;
-    tfila * fila_principal;
-	tfila * fila_impar;
-	tfila * fila_par;
+    tfila fila_principal;
+    tfila fila_impar;
+    tfila fila_par;
     criar(&fila_principal);
     criar(&fila_impar);
     criar(&fila_par);
-	
+
 	if(!enfilerar(&fila_principal, 10)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 98)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 687)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 103)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 91)){
 		printf("ERRO");
 	}
@@ -116,22 +102,23 @@ int main() {
 	if(!enfilerar(&fila_principal, 66)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 38)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 99)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 64)){
 		printf("ERRO");
 	}
-	
+
 	if(!enfilerar(&fila_principal, 32)){
 		printf("ERRO");
 	}
+
 
 	if(separar_impar_par(&fila_principal, &fila_impar, &fila_par)){
 		printf("fila principal: \n");
@@ -141,7 +128,7 @@ int main() {
 				printf("\nfila impar: \n");
 				if(imprimir_fila(&fila_impar)){
 				}
-			}	
+			}
 		}
 	}
 
