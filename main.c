9ea7049003e92387bd4003a19cb7bf9f01f11548
João Guilherme <jaaumgui@gmail.com>
2023-04-05 09:56:18 -0300

    Implementado metodo para desenfileirar uma fila separando os valores entre par e impar. Porem contem um bug ao enviar para fila impar

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ void criar (tfila * ppf){
 bool enfilerar(tfila * ppf, int valor){
     tno * novo = (tno *) malloc(sizeof (tno));
     if (novo == NULL){
+    	printf("ERRO");
         return false;
     }else {
         if (ppf->comeco == NULL) {
@@ -29,14 +30,15 @@ bool enfilerar(tfila * ppf, int valor){
             ppf->comeco = novo;
             ppf->fim = novo;
             printf("FilaPF[%d]", novo->dado);
+            return true;
         }else{
             novo->dado = valor;
             novo-> prox = NULL;
             (ppf->fim)->prox = novo;
             ppf->fim = novo;
             printf("FilaPF[%d]", novo->dado);
+            return true;
         }
-       return true;
     }
 }
 
@@ -49,23 +51,98 @@ bool desinfileirar(tfila * ppf, int * valor){
         *valor = aux->dado;
         ppf->comeco = aux->prox;
         free(aux);
+        return true;
     }
-    return true;
 }
 
-void main() {
-    int pegaValor;
-    tfila * pf;
-    criar(&pf);
-    if(!enfilerar(&pf, 1)){
-        printf("Erro ao adicionar a fila.");
-    }
-    if(!enfilerar(&pf, 2)){
-        printf("Erro ao adicionar a fila.");
-    }
-    if(desinfileirar(&pf, &pegaValor)){
-        printf("Retirado valor");
-    }
+bool separar_impar_par(tfila * fila_inicial, tfila * fila_impar, tfila * fila_par){
+	int valor;
+	while(desinfileirar(fila_inicial, &valor)){
+		if(valor%2==0){
+			if(!enfilerar(fila_par, valor)){
+				return false;
+			}
+			printf("Salvo par %d\n", valor);
+		}else if(valor%2!=0){
+			if(!enfilerar(fila_impar, valor)){	
+				return false;
+			}
+			printf("Salvo impar %d\n", valor);
+		}
+	}
+	return true;	
+}
 
+
+
+bool imprimir_fila(tfila * fila){
+	int valor;
+	while(desinfileirar(fila, &valor)){
+		printf("%d-", valor);
+	}
+	return true;
+}
+
+
+int main() {
+    int pegaValor;
+    tfila * fila_principal;
+	tfila * fila_impar;
+	tfila * fila_par;
+    criar(&fila_principal);
+    criar(&fila_impar);
+    criar(&fila_par);
+	
+	if(!enfilerar(&fila_principal, 10)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 98)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 687)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 103)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 91)){
+		printf("ERRO");
+	}
+
+	if(!enfilerar(&fila_principal, 66)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 38)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 99)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 64)){
+		printf("ERRO");
+	}
+	
+	if(!enfilerar(&fila_principal, 32)){
+		printf("ERRO");
+	}
+
+	if(separar_impar_par(&fila_principal, &fila_impar, &fila_par)){
+		printf("fila principal: \n");
+		if(imprimir_fila(&fila_principal)){
+			printf("\nfila par: \n");
+			if(imprimir_fila(&fila_par)){
+				printf("\nfila impar: \n");
+				if(imprimir_fila(&fila_impar)){
+				}
+			}	
+		}
+	}
 
 }
