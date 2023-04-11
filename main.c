5ec887897dc9ac8c75ee786d13f0a98d9cdc8705
João Guilherme <jaaumgui@gmail.com>
2023-04-11 12:21:17 -0300

    merged from repository to local minor changes

--- main.c
+++ main.c
@@ -4,42 +4,49 @@
 
 typedef struct no {
     int dado;
-    struct no *prox;
+    struct no * prox;
 } tno;
 
-typedef struct fila {
-    tno *comeco;
-    tno *fim;
-} tfila;
+typedef struct fila{
+    tno * comeco;
+    tno * fim;
+}tfila;
 
-void criar(tfila *ppf) {
+void criar (tfila * ppf){
     ppf->comeco = NULL;
     ppf->fim = NULL;
 }
 
-bool enfilerar(tfila *ppf, int valor) {
-    tno *novo = (tno *)malloc(sizeof(tno));
-    if (novo == NULL) {
-        printf("ERRO");
+bool enfilerar(tfila * ppf, int valor){
+    tno * novo = (tno *) malloc(sizeof (tno));
+    if (novo == NULL){
+    	printf("ERRO");
         return false;
-    } else {
-        novo->dado = valor;
-        novo->prox = NULL;
+    }else {
         if (ppf->comeco == NULL) {
+            novo->dado = valor;
+            novo->prox = NULL;
             ppf->comeco = novo;
-        } else {
-            ppf->fim->prox = novo;
+            ppf->fim = novo;
+            printf("FilaPF[%d]", novo->dado);
+            return true;
+        }else{
+            novo->dado = valor;
+            novo-> prox = NULL;
+            (ppf->fim)->prox = novo;
+            ppf->fim = novo;
+            printf("FilaPF[%d]", novo->dado);
+            return true;
         }
-        ppf->fim = novo;
-        return true;
     }
 }
 
-bool desinfileirar(tfila *ppf, int *valor) {
-    if (ppf->comeco == NULL) {
+bool desinfileirar(tfila * ppf, int * valor){
+    tno * aux;
+    if(ppf->comeco==NULL){
         return false;
-    } else {
-        tno *aux = ppf->comeco;
+    }else{
+        aux = ppf->comeco;
         *valor = aux->dado;
         ppf->comeco = aux->prox;
         free(aux);
@@ -63,13 +70,16 @@ bool separar_impar_par(tfila *fila_inicial, tfila *fila_impar, tfila *fila_par) 
     return true;
 }
 
-bool imprimir_fila(tfila *fila) {
-    int valor;
-    while (desinfileirar(fila, &valor)) {
-        printf("%d-", valor);
-    }
-    return true;
+
+
+bool imprimir_fila(tfila * fila){
+	int valor;
+	while(desinfileirar(fila, &valor)){
+		printf("%d-", valor);
+	}
+	return true;
 }
+
 
 int main() {
     tfila fila_principal;
@@ -78,23 +88,23 @@ int main() {
     criar(&fila_principal);
     criar(&fila_impar);
     criar(&fila_par);
-
+	
 	if(!enfilerar(&fila_principal, 10)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 98)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 687)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 103)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 91)){
 		printf("ERRO");
 	}
@@ -102,23 +112,22 @@ int main() {
 	if(!enfilerar(&fila_principal, 66)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 38)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 99)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 64)){
 		printf("ERRO");
 	}
-
+	
 	if(!enfilerar(&fila_principal, 32)){
 		printf("ERRO");
 	}
-
 
 	if(separar_impar_par(&fila_principal, &fila_impar, &fila_par)){
 		printf("fila principal: \n");
@@ -128,7 +137,7 @@ int main() {
 				printf("\nfila impar: \n");
 				if(imprimir_fila(&fila_impar)){
 				}
-			}
+			}	
 		}
 	}
 
